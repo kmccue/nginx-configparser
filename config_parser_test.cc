6c2b90f93835475f03139bf6989b72464858f9f2
Kayla McCue <kmccue@caltech.edu>
2015-04-16 18:18:57 -0700

    Added Unit Tests in unit_test directory.

--- config_parser_test.cc
+++ config_parser_test.cc
@@ -9,3 +9,39 @@ TEST(NginxConfigParserTest, SimpleConfig) {
 
   EXPECT_TRUE(success);
 }
+
+TEST(NginxConfigParserTest, EmptyFile) {
+  NginxConfigParser parser;
+  NginxConfig out_config;
+
+  bool success = parser.Parse("unit_tests/empty_file.txt", &out_config);
+
+  EXPECT_FALSE(success);
+}
+
+TEST(NginxConfigParserTest, EmptyBlock) {
+  NginxConfigParser parser;
+  NginxConfig out_config;
+
+  bool success = parser.Parse("unit_tests/empty_block.txt", &out_config);
+
+  EXPECT_FALSE(success);
+}
+
+TEST(NginxConfigParserTest, hipsterIpsum) {
+  NginxConfigParser parser;
+  NginxConfig out_config;
+
+  bool success = parser.Parse("unit_tests/hipster_ipsum.txt", &out_config);
+
+  EXPECT_FALSE(success);
+}
+
+TEST(NginxConfigParserTest, noListenPort) {
+  NginxConfigParser parser;
+  NginxConfig out_config;
+
+  bool success = parser.Parse("unit_tests/no_listen_port.txt", &out_config);
+
+  EXPECT_FALSE(success);
+}
